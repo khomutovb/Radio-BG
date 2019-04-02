e662fca5a68e814852b4cd35f4fe5f7e61226360
Your Name <khomutovb@gmail.com>
2019-04-02 22:40:52 +0300

    BG

--- js/main.js
+++ js/main.js
@@ -19,16 +19,27 @@ $(document).ready(function() {
 		
 	});
  $("#opensc").click(function(){
-        var height = $( 'body' ).height();
-        $( '.wrapper' ).css("top",height); 
 	$(".wrapper").toggleClass('opened'); 
+    if($(".wrapper").hasClass('opened')) {
+        $("#opensc").html('Close soundcloud');
+        var height = $(document).height();
+        $(height) + $(".wrapper").css("top", height);
+    }
+    else {
+        $("#opensc").html('Open soundcloud');
+        $(".wrapper").css("top", 0);
+        
+    }
+});
+/* $("#opensc").click(function(){
+	$(".wrapper").toggleClass('opened');
     if($(".wrapper").hasClass('opened')) {
         $("#opensc").html('Close soundcloud');
     }
     else {
         $("#opensc").html('Open soundcloud');
     }
-});
+}); */
 $("#opensc").click(function(){
 	var elementClick = $(this).attr("href");
 	var destination = $(elementClick).offset().top;
